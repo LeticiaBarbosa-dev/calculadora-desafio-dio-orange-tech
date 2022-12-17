020be94fc41649e086f882af291c477d51daf049
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-12-17 20:54:54 -0200

    fix(button): fix button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 
 export function Button({label, onClick}: ButtonProps) {
     return (
-        <button onClick={onClick} className="p-5 border-[1px] border-[#CDCDCD] bg-[#3e3e3e] text-white text-2xl font-bold flex-1 hover:opacity-60 dark:text-white dark:bg-[#dbdbdb]">
+        <button onClick={onClick} className="p-5 border-[1px] border-[#CDCDCD] bg-[#3e3e3e] text-white text-2xl font-bold flex-1 hover:opacity-60 dark:text-white">
             {label}
         </button>
     )
